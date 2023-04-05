2ab71ebd5bcc7a61af08e54ed596dfb2b702b482
Abdullah Hejazi <a.m.a.hejazi@gmail.com>
2023-04-05 23:29:19 +0200

    added starts at

--- src/bspextractor.js
+++ src/bspextractor.js
@@ -1,6 +1,18 @@
 const BSP = require('./bsp');
 
+const START_LETTER = 'p';
 BSP.walkDir('/maps/pk3/', async (filePath) => {
+    let file = filePath.split('/').pop();
+    let ex = false;
+
+    if (file.toLowerCase().startsWith(START_LETTER)) {
+        ex = true;
+    }
+
+    if (! ex) {
+        return;
+    }
+
     if (filePath.endsWith(".pk3")) {
         console.log('Extracting: ' + filePath)
 
